8ebad85fa8854cd6f790a3a27637d9c635dec9b5
Capi Etheriel <barraponto@gmail.com>
2011-08-09 10:31:19 -0300

    unified PROJECT_EXTENDED variable

--- newfeature.sh
+++ newfeature.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #change the variables to suit your project
+PROJECT_EXTENDED="Tecido Starter"
 PROJECT="tecidoSTARTER"
 #prefix the first argument for the feature name
 FEATURE="${PROJECT}_${1}"
@@ -13,5 +14,4 @@ fi
 #now copy, rename and sed the new feature
 cp -r tecidoSTARTER_feature modules/$PROJECT/$FEATURE
 rename "s/tecidoSTARTER_feature/$FEATURE/" modules/$PROJECT/$FEATURE/*
-sed -i "s/tecidoSTARTER_feature/${FEATURE}/g" modules/$PROJECT/$FEATURE/*
-sed -i "s/TecidoSTARTER/${FEATURE}/g" modules/$PROJECT/$FEATURE/*
+sed -i -e "s/tecidoSTARTER_feature/$FEATURE/g" -e "s/Tecido Starter/$PROJECT_EXTENDED/g" modules/$PROJECT/$FEATURE/*
